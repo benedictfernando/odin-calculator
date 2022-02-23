9f3adb5aaa960c978a9e44419a8abab494bd4a4c
benedict <fernandobenedict619@gmail.com>
2022-02-23 23:02:47 +0800

    make number & operation buttons work

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ let operators = [...document.getElementsByClassName('operator')];
 let reset = document.getElementById('reset');
 let equals = document.getElementById('equals');
 
+let firstOperand, secondOperand, firstOperator, secondOperator;
+let isOperatorClicked = false;
+
 
 numbers.forEach(number => {
   number.addEventListener('click', numberClick);
@@ -15,27 +18,96 @@ operators.forEach(operator => {
   operator.addEventListener('click', operatorClick);
 });
 
+document.addEventListener('keydown', keypress);
+
 reset.addEventListener('click', clear);
 
 
 // For number buttons
-function numberClick(e) {
-  if (operand.textContent == 0) operand.textContent = e.target.textContent;
-  else operand.textContent += e.target.textContent;
+function numberClick(e) 
+{
+  if (operand.textContent.length < 8 || isOperatorClicked) 
+  {
+    if (operand.textContent == 0 || isOperatorClicked)  
+      operand.textContent = e.target.textContent;
+    else operand.textContent += e.target.textContent;
+  
+    isOperatorClicked = false;
+  }
 }
 
 // For operator buttons
-function operatorClick(e) {
-  console.log(e.target.textContent);
-}
+function operatorClick(e) 
+{
+  if (!isOperatorClicked) evaluate(e.target.textContent);
+  else changeOperator(e.target.textContent);
 
-// For clear button
-function clear() {
-  operand.textContent = '0';
-  expression.innerHTML = '&nbsp;';
+  isOperatorClicked = true;
 }
 
 // For equals button
-function evaluate() {
-  // to-do
+function evaluate(operator) 
+{
+  let operandText = operand.textContent;
+
+  if (!firstOperand && !secondOperand) {
+    firstOperand = parseInt(operandText);
+    firstOperator = operator;
+  } else if (firstOperand && !secondOperand) {
+    secondOperand = parseInt(operandText);
+    secondOperator = operator;
+  }
+
+  if (firstOperand && secondOperand) operate();
+  expression.textContent += `${operandText} ${firstOperator} `;
+}
+
+function changeOperator(operator) {
+  firstOperator = operator;
+  expression.textContent = expression.textContent.slice(0, -2) + operator + ' ';
+}
+
+function operate() 
+{
+  switch (firstOperator) {
+    case '+':
+      firstOperand = firstOperand + secondOperand;
+      break;
+    case '-':
+      firstOperand = firstOperand - secondOperand;
+      break;
+    case '*':
+      firstOperand = firstOperand * secondOperand;
+      break;
+    case '/':
+      firstOperand = firstOperand / secondOperand;
+      break;   
+  }
+
+  operand.textContent = firstOperand;
+  firstOperator = secondOperator;
+  secondOperand = secondOperator = null;
+}
+
+// For clear button
+function clear() 
+{
+  operand.textContent = '0';
+  expression.innerHTML = '&nbsp;';
+
+  firstOperand = firstOperator = secondOperand = secondOperator = null;
+  isOperatorClicked = false;
+}
+
+function keypress(e) 
+{
+  switch (e.key) {
+    case "Backspace":
+      operand.textContent = operand.textContent.slice(0, -1); 
+      if (!operand.textContent) operand.textContent = '0';
+      break;
+    case "Enter":
+      console.log("lez g");
+      break;
+  }
 }
